3c413a13cf8da1f73e1a34ab3ab79256c28431f5
ajays <sahu.ajaykumar4@gmail.com>
2020-05-20 21:42:34 +0530

    code cleanup

--- src/context/EventsContext.js
+++ src/context/EventsContext.js
@@ -3,13 +3,7 @@ import React, { useState, createContext } from 'react'
 export const EventsContext =  createContext();
 
 export const EventProvider = (props) => {
-  const [events, setEvents] = useState(
-    [
-      { name: 'Cycling Event', price: "5000", id: '23123'},
-      { name: 'Run a marathon', price: "$20", id: '23124'},
-      { name: 'Covid support', price: "$30", id: '23125'}
-  ]);
-
+  const [events, setEvents] = useState([]);
   const [showEvents, setShowEvents] = useState(false);
 
 
